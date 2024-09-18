85c90ae72cd4c077cc88a4c2071a96ff2b0d4a25
Timothy <timothy96108@gmail.com>
2024-09-18 17:16:51 +0800

    Updates

---
+++ assets/meditate-complete-551d0c4f.js
@@ -0,0 +1,1 @@
+import"./main-448b7643.js";import{P as r}from"./progressbar.min-b6ae59e6.js";var t=new r.Circle("#static-progress-circle",{strokeWidth:4,color:"#ffcc00",trailColor:"#eee",trailWidth:4,svgStyle:null,duration:0});t.set(1);
